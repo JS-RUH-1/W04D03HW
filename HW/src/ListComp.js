01b70333eb705fd60364ea0a58c1aa1a0fc332c0
aalsbr <a.alsbr40@gmail.com>
2021-10-26 16:37:41 +0300

    Style Added

--- HW/src/ListComp.js
+++ HW/src/ListComp.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css"
 
 function ListComp() {
   const [mylist, setmylist] = useState([]);
@@ -31,6 +32,8 @@ function ListComp() {
   }
 
   const addBtnClicked = () => {
+    if(addList=="")
+    return
     //create object of counters
     setCounter(counter.concat({ id: counter.length, count: 0 }));
     //copy current list
@@ -55,39 +58,42 @@ function ListComp() {
   };
 
   return (
-    <div>
+    <div className="continer">
       <h1>React List Add</h1>
       <ul>
+
         {mylist.map((e, index) => (
-          <li key={index}>
-            {e}
-            <button onClick={() => deleteBtnClicke(index)}>Delete</button>
-            <button
+
+        <li style={{fontSize:22,fontWeight:"bold",listStyleType:'none'}} key={index}>
+            { e }
+            <button className="deletBtn" onClick={() => deleteBtnClicke(index)}>Delete</button>
+            <button className="add"
               onClick={() => {
                 increase(index);
               }}
             >
               +
             </button>
-            <span>{counter[index].count}</span>
-            <button
+            <span class="counternum">{counter[index].count}</span>
+            <button className="decress"
               onClick={() => {
                 decrease(index);
               }}
             >
               -
             </button>
+            <hr/>
           </li>
         ))}
       </ul>
-      <h4> What needs to be done:</h4>
+      <h2> What needs to be done:</h2>
 
-      <input
+      <input  style={{padding:10,fontSize:20,textIndent:"40%"}}
         type="text"
         value={addList}
         onChange={(e) => setaddList(e.target.value)}
       />
-      <button onClick={() => addBtnClicked()}>Add</button>
+      <button style={{padding:10,fontSize:20}} onClick={() => addBtnClicked()}>Add</button>
     </div>
   );
 }
